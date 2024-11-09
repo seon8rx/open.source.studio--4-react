b30eb454a0a31711a7f0af553e87f65dffb80104
SUNGKYU KIM <seooonggyu@gmail.com>
2024-11-09 20:37:23 +0900

    app.js

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const click = () => {
+    alert("버튼이 눌렸습니다.");
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +12,7 @@ function App() {
         <p>
           <h1>22100110 김성규</h1>
         </p>
-        <button className="mybutton">추가된 버튼</button>
+        <button className="mybutton" onClick={click}>추가된 버튼</button>
       </header>
       <footer className="myfooter">
         <p>오픈소스스튜디오</p>
